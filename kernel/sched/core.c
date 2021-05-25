72d0086e7e47cef11ae69f5aa356cb76951ef756
Reinazhard <reinazhard@gmail.com>
2021-05-25 02:52:05 +0000

    kernel/sched: update & fix
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -276,15 +276,15 @@ int sysctl_sched_rt_runtime = 950000;
 cpumask_var_t cpu_isolated_map;
 
 struct rq *
-lock_rq_of(struct task_struct *p, unsigned long *flags)
+lock_rq_of(struct task_struct *p,  struct rq_flags *rf)
 {
-	return task_rq_lock(p, flags);
+	return task_rq_lock(p, rf);
 }
 
 void
-unlock_rq_of(struct rq *rq, struct task_struct *p, unsigned long *flags)
+unlock_rq_of(struct rq *rq, struct task_struct *p, struct rq_flags *rf)
 {
-	task_rq_unlock(rq, p, flags);
+	task_rq_unlock(rq, p, rf);
 }
 
 /*
