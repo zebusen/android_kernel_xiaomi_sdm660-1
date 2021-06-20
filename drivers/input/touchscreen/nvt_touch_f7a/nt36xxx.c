silont-project/android_kernel_xiaomi_sdm660

Revert "input: touchscreen: Remove unused longcheer extension"
This reverts commit 2f1392d78782ea615a26cf5e858ae0c9c8ed3363.

drivers/input/touchscreen/nvt_touch_f7a/nt36xxx.c
@@ -41,9 +41,18 @@
 #include <linux/jiffies.h>
 #endif /* #if NVT_TOUCH_ESD_PROTECT */
 
+#ifdef CONFIG_KERNEL_CUSTOM_FACTORY
+#include "../lct_tp_work.h"
+#endif
+#include "../lct_tp_gesture.h"
+#include "../lct_tp_grip_area.h"
+
 /* add verify LCD by wanghan start */
 extern char g_lcd_id[128];
 /* add verify LCD by wanghan end */
+/* add touchpad information by wanghan start */
+extern int lct_nvt_tp_info_node_init(void);
+/* add touchpad information by wanghan end */
 /* add resume work by wanghan start */
 static struct work_struct g_resume_work;
 /* add resume work by wanghan end */
@@ -82,6 +91,13 @@ static struct workqueue_struct *nvt_wq;
 static struct workqueue_struct *nvt_fwu_wq;
 extern void Boot_Update_Firmware(struct work_struct *work);
 #endif
+
+#ifdef CONFIG_KERNEL_CUSTOM_FACTORY
+static int lct_tp_work_node_callback(bool flag);
+#endif
+static int lct_tp_gesture_node_callback(bool flag);
+static int lct_tp_get_screen_angle_callback(void);
+static int lct_tp_set_screen_angle_callback(unsigned int angle);
 
 /* add resume work by wanghan start */
 static void do_nvt_ts_resume_work(struct work_struct *work);
@@ -1581,6 +1597,29 @@ static int32_t nvt_ts_probe(struct i2c_client *client, const struct i2c_device_i
 			msecs_to_jiffies(NVT_TOUCH_ESD_CHECK_PERIOD));
 #endif /* #if NVT_TOUCH_ESD_PROTECT */
 
+
+
+#ifdef CONFIG_KERNEL_CUSTOM_FACTORY
+	ret = init_lct_tp_work(lct_tp_work_node_callback);
+	if (ret < 0) {
+		NVT_ERR("Failed to add /proc/tp_work node!\n");
+	}
+#endif
+
+	ret = init_lct_tp_gesture(lct_tp_gesture_node_callback);
+	if (ret < 0) {
+		NVT_ERR("Failed to add /proc/tp_work node!\n");
+	}
+
+	ret = init_lct_tp_grip_area(lct_tp_set_screen_angle_callback, lct_tp_get_screen_angle_callback);
+	if (ret < 0) {
+		NVT_ERR("Failed to add /proc/tp_grip_area node!\n");
+	}
+
+	/* add touchpad information by wanghan start */
+	lct_nvt_tp_info_node_init();
+	/* add touchpad information by wanghan end */
+
 #if NVT_TOUCH_PROC
 	ret = nvt_flash_proc_init();
 	if (ret != 0) {
@@ -1860,6 +1899,138 @@ static void do_nvt_ts_resume_work(struct work_struct *work)
 }
 /* add resume work by wanghan end */
 
+#ifdef CONFIG_KERNEL_CUSTOM_FACTORY
+static int lct_tp_work_node_callback(bool flag)
+{
+	int ret = 0;
+
+	LOG_ENTRY();
+	if (enable_gesture_mode) {
+		LOGV("ERROR: novatek gesture=%d!\n", enable_gesture_mode);
+		return -1;
+	}
+	if (suspend_state) return 0;
+	if (flag) {
+		suspend_state = true;
+		ret = nvt_ts_resume(&ts->client->dev);
+		if (ret < 0) NVT_ERR("nvt_ts_resume faild! ret=%d\n", ret);
+	} else {
+		ret = nvt_ts_suspend(&ts->client->dev);
+		if (ret < 0) NVT_ERR("nvt_ts_suspend faild! ret=%d\n", ret);
+	}
+	suspend_state = false;
+
+	LOG_DONE();
+	return ret;
+}
+#endif
+
+static int lct_tp_gesture_node_callback(bool flag)
+{
+	int retval = 0;
+
+	LOG_ENTRY();
+	if (suspend_state) {
+		LOGV("ERROR: TP is suspend!\n");
+		return -1;
+	}
+	if(flag) {
+		enable_gesture_mode = true;
+		LOGV("enable gesture mode\n");
+	} else {
+		enable_gesture_mode = false;
+		LOGV("disable gesture mode\n");
+	}
+	LOG_DONE();
+	return retval;
+}
+
+static int lct_tp_get_screen_angle_callback(void)
+{
+	uint8_t tmp[8] = {0};
+	int32_t ret = -EIO;
+	uint8_t edge_reject_switch;
+
+	if ( !bTouchIsAwake || (ts->touch_state != TOUCH_STATE_WORKING) ) {
+		NVT_ERR("tp is suspended or flashing, can not to set\n");
+		return ret;
+	}
+
+	NVT_LOG("++\n");
+
+	mutex_lock(&ts->lock);
+
+	msleep(35);
+
+	tmp[0] = 0xFF;
+	tmp[1] = (ts->mmap->EVENT_BUF_ADDR >> 16) & 0xFF;
+	tmp[2] = (ts->mmap->EVENT_BUF_ADDR >> 8) & 0xFF;
+	ret = CTP_I2C_WRITE(ts->client, I2C_FW_Address, tmp, 3);
+	if (ret < 0) {
+		NVT_ERR("Set event buffer index fail!\n");
+		goto out;
+	}
+
+	tmp[0] = 0x5C;
+	tmp[1] = 0x00;
+	ret = CTP_I2C_READ(ts->client, I2C_FW_Address, tmp, 2);
+	if (ret < 0) {
+		NVT_ERR("Read edge reject switch status fail!\n");
+		goto out;
+	}
+
+	edge_reject_switch = ((tmp[1] >> 5) & 0x03);
+	NVT_LOG("edge_reject_switch = %d\n", edge_reject_switch);
+	ret = edge_reject_switch;
+
+out:
+	mutex_unlock(&ts->lock);
+	NVT_LOG("--\n");
+	return ret;
+}
+static int lct_tp_set_screen_angle_callback(unsigned int angle)
+{
+	uint8_t tmp[3];
+	int ret = -EIO;
+
+	if ( !bTouchIsAwake || (ts->touch_state != TOUCH_STATE_WORKING) ) {
+		NVT_ERR("tp is suspended or flashing, can not to set\n");
+		return ret;
+	}
+
+	NVT_LOG("++\n");
+
+	mutex_lock(&ts->lock);
+
+	tmp[0] = 0XFF;
+	tmp[1] = (ts->mmap->EVENT_BUF_ADDR >> 16) & 0xFF;
+	tmp[2] = (ts->mmap->EVENT_BUF_ADDR >> 8) & 0xFF;
+	ret = CTP_I2C_WRITE(ts->client, I2C_FW_Address, tmp, 3);
+	if (ret < 0) {
+		NVT_LOG("i2c wite error!\n");
+		goto out;
+	}
+	tmp[0] = EVENT_MAP_HOST_CMD;
+	if (angle == 90) {
+		tmp[1] = 0xBC;
+	} else if (angle == 270) {
+		tmp[1] = 0xBB;
+	} else {
+		tmp[1] = 0xBA;
+	}
+	ret = CTP_I2C_WRITE(ts->client, I2C_FW_Address, tmp, 2);
+	if (ret < 0) {
+		NVT_LOG("i2c read error!\n");
+		goto out;
+	}
+	ret = 0;
+
+out:
+	mutex_unlock(&ts->lock);
+	NVT_LOG("--\n");
+	return ret;
+}
+
 #if defined(CONFIG_FB)
 static int fb_notifier_callback(struct notifier_block *self, unsigned long event, void *data)
 {
@@ -1873,6 +2044,12 @@ static int fb_notifier_callback(struct notifier_block *self, unsigned long event
 		blank = evdata->data;
 		if (*blank == FB_BLANK_POWERDOWN) {
 			NVT_LOG("touch suspend\n");
+#ifdef CONFIG_KERNEL_CUSTOM_FACTORY
+			if (!get_lct_tp_work_status()) {
+				suspend_state = true;
+				return 0;
+			}
+#endif
 			flush_work(&g_resume_work);
 			mutex_lock(&ts->pm_mutex);
 			nvt_ts_suspend(&ts->client->dev);
@@ -1882,6 +2059,12 @@ static int fb_notifier_callback(struct notifier_block *self, unsigned long event
 		blank = evdata->data;
 		if (*blank == FB_BLANK_UNBLANK) {
 			NVT_LOG("touch resume\n");
+#ifdef CONFIG_KERNEL_CUSTOM_FACTORY
+			if (!get_lct_tp_work_status()) {
+				suspend_state = false;
+				return 0;
+			}
+#endif
 			schedule_work(&g_resume_work);
 		}
 	}

drivers/input/touchscreen/synaptics_tcm/synaptics_tcm_reflash.c
@@ -34,6 +34,14 @@
 #include <linux/firmware.h>
 #include "synaptics_tcm_core.h"
 
+/* add tp vendor information by wanghan add */
+#include "../lct_tp_info.h"
+/* add tp vendor information by wanghan end */
+
+/* add check F7A LCM by wanghan start */
+extern bool lct_syna_verify_flag;
+/* add check F7A LCM by wanghan end */
+
 #define STARTUP_REFLASH
 
 #define FORCE_REFLASH false
@@ -1934,6 +1942,27 @@ reflash_update(app_firmware)
 
 #define SYNA_TP_INFO_SIZE       128
 
+static int lct_syna_tp_info_update(struct syna_tcm_hcd *tcm_hcd)
+{
+	char tp_info_buf[SYNA_TP_INFO_SIZE];
+	unsigned char *image_config_id;
+	struct app_config_header *header;
+	const unsigned char *app_config_data;
+
+	LOG_ENTRY();
+	app_config_data = reflash_hcd->image_info.app_config.data;
+	header = (struct app_config_header *)app_config_data;
+	image_config_id = header->customer_config_id;
+	memset(tp_info_buf, 0, sizeof(tp_info_buf));
+	sprintf(tp_info_buf, "[Vendor]boe,[FW]0x%c%c,[IC]td4320\n",
+			image_config_id[14],
+			image_config_id[15]);
+	update_lct_tp_info(tp_info_buf, NULL);
+	LOG_DONE();
+	return 0;
+}
+/* add tp vendor information by wanghan end */
+
 static int reflash_do_reflash(void)
 {
 	int retval;
@@ -1996,6 +2025,12 @@ static int reflash_do_reflash(void)
 
 	LOGN(tcm_hcd->pdev->dev.parent,
 			"End of reflash\n");
+	/* add tp vendor information by wanghan add */
+	if (update_area != NONE) {
+		lct_syna_tp_info_update(tcm_hcd);
+		LOGV("Update /proc/tp_info\n");
+	}
+	/* add tp vendor information by wanghan end */
 
 	retval = 0;
 
@@ -2011,12 +2046,134 @@ exit:
 	return retval;
 }
 
+/* add tp vendor information by wanghan add */
+static int lct_syna_tp_lockdown_info_update(char *buf, struct syna_tcm_hcd *tcm_hcd)
+{
+	int retval;
+
+	LOG_ENTRY();
+
+	mutex_lock(&tcm_hcd->extif_mutex);
+	mutex_lock(&reflash_hcd->reflash_mutex);
+
+	retval = reflash_read_data(CUSTOM_OTP, true, NULL);
+	if (retval < 0) {
+		LOGE(tcm_hcd->pdev->dev.parent,
+				"Failed to read lockdown data\n");
+		goto exit;
+	}
+	sprintf(buf, "%02x%02x%02x%02x%02x%02x%02x\n",
+			(uint8_t)(reflash_hcd->read.buf[0]),
+			(uint8_t)(reflash_hcd->read.buf[1]),
+			(uint8_t)(reflash_hcd->read.buf[2]),
+			(uint8_t)(reflash_hcd->read.buf[3]),
+			(uint8_t)(reflash_hcd->read.buf[4]),
+			(uint8_t)(reflash_hcd->read.buf[5]),
+			(uint8_t)(reflash_hcd->read.buf[6]));
+	LOGV("get lockdown info : %s\n", buf);
+
+exit:
+	mutex_unlock(&reflash_hcd->reflash_mutex);
+	mutex_unlock(&tcm_hcd->extif_mutex);
+	LOG_DONE();
+	return 0;
+}
+
+
+#define LCT_SYNA_TP_LOCKDOWN_CALLBACK_RUN_ONLY_ONCE 1
+
+int lct_syna_tp_callback(const char *cmd)
+{
+#if LCT_SYNA_TP_LOCKDOWN_CALLBACK_RUN_ONLY_ONCE
+	static bool run_only_once_flag = false;
+#endif
+	int retval;
+	char tp_lockdown_info_buf[SYNA_TP_INFO_SIZE];
+	struct syna_tcm_hcd *tcm_hcd = reflash_hcd->tcm_hcd;
+
+	LOG_ENTRY();
+
+#if LCT_SYNA_TP_LOCKDOWN_CALLBACK_RUN_ONLY_ONCE
+	if (run_only_once_flag) return 0;
+#endif
+	if(strcmp(cmd, TP_CALLBACK_CMD_LOCKDOWN))
+		return -1;
+	memset(tp_lockdown_info_buf, 0, sizeof(tp_lockdown_info_buf));
+	retval = lct_syna_tp_lockdown_info_update(tp_lockdown_info_buf, tcm_hcd);
+	if (retval < 0) {
+		LOGE(tcm_hcd->pdev->dev.parent, "failed to get tp lockdown info!\n");
+		return retval;
+	}
+	update_lct_tp_info(NULL, tp_lockdown_info_buf);
+#if LCT_SYNA_TP_LOCKDOWN_CALLBACK_RUN_ONLY_ONCE
+	run_only_once_flag = true;
+#endif
+
+	LOG_DONE();
+	return 0;
+}
+
+static int lct_syna_tp_node_init(struct syna_tcm_hcd *tcm_hcd)
+{
+	int retval;
+	char tp_info_buf[SYNA_TP_INFO_SIZE];
+
+	LOG_ENTRY();
+
+	memset(tp_info_buf, 0, sizeof(tp_info_buf));
+	sprintf(tp_info_buf, "[Vendor]boe,[FW]0x%c%c,[IC]td4320\n",
+			tcm_hcd->app_info.customer_config_id[14],
+			tcm_hcd->app_info.customer_config_id[15]);
+	retval = init_lct_tp_info(tp_info_buf, NULL);
+	if (retval < 0) {
+		LOGE(tcm_hcd->pdev->dev.parent, "init_lct_tp_info is failed!\n");
+		return -1;
+	}
+	retval = lct_syna_tp_callback(TP_CALLBACK_CMD_LOCKDOWN);
+	if (retval) {
+		LOGV("ERROR : lct_syna_tp_callback failed!\n");
+	}
+	set_lct_tp_info_callback(lct_syna_tp_callback);
+	LOG_DONE();
+	return 0;
+}
+/* add tp vendor information by wanghan end */
+
 #ifdef STARTUP_REFLASH
 static void reflash_startup_work(struct work_struct *work)
 {
 	int retval;
+
+
+
 	struct syna_tcm_hcd *tcm_hcd = reflash_hcd->tcm_hcd;
+
 	LOG_ENTRY();
+	/* add tp vendor information by wanghan add */
+	retval = lct_syna_tp_node_init(tcm_hcd);
+	if (retval < 0){
+		LOGE(tcm_hcd->pdev->dev.parent,
+				"Failed to initializing tp_info & tp_lockdown_info node\n");
+	}
+	/* add tp vendor information by wanghan end */
+
+#ifdef CONFIG_KERNEL_CUSTOM_FACTORY
+	LOGV("Close the TP FW upgrade feature in longcheer factory version\n");
+	return;
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	pm_stay_awake(&tcm_hcd->pdev->dev);
 
@@ -2240,6 +2397,10 @@ static int __init reflash_module_init(void)
 {
 	int retval;
 	LOG_ENTRY();
+	/* add check F7A LCM by wanghan start */
+	if(!lct_syna_verify_flag)
+		return -ENODEV;
+	/* add check F7A LCM by wanghan end */
 	LOGV("__init reflash module\n");
 	retval = syna_tcm_add_module(&reflash_module, true);
 	if (retval < 0)

zebusen/android_kernel_xiaomi_sdm660-1

drivers/input/touchscreen/nvt_touch_f7a/nt36xxx.c
@@ -41,9 +41,18 @@
 #include <linux/jiffies.h>
 #endif /* #if NVT_TOUCH_ESD_PROTECT */
 
+#ifdef CONFIG_MACH_MI
+#include "../lct_tp_work.h"
+#endif
+#include "../lct_tp_gesture.h"
+#include "../lct_tp_grip_area.h"
+
 /* add verify LCD by wanghan start */
 extern char g_lcd_id[128];
 /* add verify LCD by wanghan end */
+/* add touchpad information by wanghan start */
+extern int lct_nvt_tp_info_node_init(void);
+/* add touchpad information by wanghan end */
 /* add resume work by wanghan start */
 static struct work_struct g_resume_work;
 /* add resume work by wanghan end */
@@ -82,6 +91,13 @@ static struct workqueue_struct *nvt_wq;
 static struct workqueue_struct *nvt_fwu_wq;
 extern void Boot_Update_Firmware(struct work_struct *work);
 #endif
+
+#ifdef CONFIG_MACH_MI
+static int lct_tp_work_node_callback(bool flag);
+#endif
+static int lct_tp_gesture_node_callback(bool flag);
+static int lct_tp_get_screen_angle_callback(void);
+static int lct_tp_set_screen_angle_callback(unsigned int angle);
 
 /* add resume work by wanghan start */
 static void do_nvt_ts_resume_work(struct work_struct *work);
@@ -1522,6 +1538,29 @@ static int32_t nvt_ts_probe(struct i2c_client *client, const struct i2c_device_i
 			msecs_to_jiffies(NVT_TOUCH_ESD_CHECK_PERIOD));
 #endif /* #if NVT_TOUCH_ESD_PROTECT */
 
+
+
+#ifdef CONFIG_MACH_MI
+	ret = init_lct_tp_work(lct_tp_work_node_callback);
+	if (ret < 0) {
+		NVT_ERR("Failed to add /proc/tp_work node!\n");
+	}
+#endif
+
+	ret = init_lct_tp_gesture(lct_tp_gesture_node_callback);
+	if (ret < 0) {
+		NVT_ERR("Failed to add /proc/tp_work node!\n");
+	}
+
+	ret = init_lct_tp_grip_area(lct_tp_set_screen_angle_callback, lct_tp_get_screen_angle_callback);
+	if (ret < 0) {
+		NVT_ERR("Failed to add /proc/tp_grip_area node!\n");
+	}
+
+	/* add touchpad information by wanghan start */
+	lct_nvt_tp_info_node_init();
+	/* add touchpad information by wanghan end */
+
 #if NVT_TOUCH_PROC
 	ret = nvt_flash_proc_init();
 	if (ret != 0) {
@@ -1801,6 +1840,138 @@ static void do_nvt_ts_resume_work(struct work_struct *work)
 }
 /* add resume work by wanghan end */
 
+#ifdef CONFIG_MACH_MI
+static int lct_tp_work_node_callback(bool flag)
+{
+	int ret = 0;
+
+	LOG_ENTRY();
+	if (enable_gesture_mode) {
+		LOGV("ERROR: novatek gesture=%d!\n", enable_gesture_mode);
+		return -1;
+	}
+	if (suspend_state) return 0;
+	if (flag) {
+		suspend_state = true;
+		ret = nvt_ts_resume(&ts->client->dev);
+		if (ret < 0) NVT_ERR("nvt_ts_resume faild! ret=%d\n", ret);
+	} else {
+		ret = nvt_ts_suspend(&ts->client->dev);
+		if (ret < 0) NVT_ERR("nvt_ts_suspend faild! ret=%d\n", ret);
+	}
+	suspend_state = false;
+
+	LOG_DONE();
+	return ret;
+}
+#endif
+
+static int lct_tp_gesture_node_callback(bool flag)
+{
+	int retval = 0;
+
+	LOG_ENTRY();
+	if (suspend_state) {
+		LOGV("ERROR: TP is suspend!\n");
+		return -1;
+	}
+	if(flag) {
+		enable_gesture_mode = true;
+		LOGV("enable gesture mode\n");
+	} else {
+		enable_gesture_mode = false;
+		LOGV("disable gesture mode\n");
+	}
+	LOG_DONE();
+	return retval;
+}
+
+static int lct_tp_get_screen_angle_callback(void)
+{
+	uint8_t tmp[8] = {0};
+	int32_t ret = -EIO;
+	uint8_t edge_reject_switch;
+
+	if ( !bTouchIsAwake || (ts->touch_state != TOUCH_STATE_WORKING) ) {
+		NVT_ERR("tp is suspended or flashing, can not to set\n");
+		return ret;
+	}
+
+	NVT_LOG("++\n");
+
+	mutex_lock(&ts->lock);
+
+	msleep(35);
+
+	tmp[0] = 0xFF;
+	tmp[1] = (ts->mmap->EVENT_BUF_ADDR >> 16) & 0xFF;
+	tmp[2] = (ts->mmap->EVENT_BUF_ADDR >> 8) & 0xFF;
+	ret = CTP_I2C_WRITE(ts->client, I2C_FW_Address, tmp, 3);
+	if (ret < 0) {
+		NVT_ERR("Set event buffer index fail!\n");
+		goto out;
+	}
+
+	tmp[0] = 0x5C;
+	tmp[1] = 0x00;
+	ret = CTP_I2C_READ(ts->client, I2C_FW_Address, tmp, 2);
+	if (ret < 0) {
+		NVT_ERR("Read edge reject switch status fail!\n");
+		goto out;
+	}
+
+	edge_reject_switch = ((tmp[1] >> 5) & 0x03);
+	NVT_LOG("edge_reject_switch = %d\n", edge_reject_switch);
+	ret = edge_reject_switch;
+
+out:
+	mutex_unlock(&ts->lock);
+	NVT_LOG("--\n");
+	return ret;
+}
+static int lct_tp_set_screen_angle_callback(unsigned int angle)
+{
+	uint8_t tmp[3];
+	int ret = -EIO;
+
+	if ( !bTouchIsAwake || (ts->touch_state != TOUCH_STATE_WORKING) ) {
+		NVT_ERR("tp is suspended or flashing, can not to set\n");
+		return ret;
+	}
+
+	NVT_LOG("++\n");
+
+	mutex_lock(&ts->lock);
+
+	tmp[0] = 0XFF;
+	tmp[1] = (ts->mmap->EVENT_BUF_ADDR >> 16) & 0xFF;
+	tmp[2] = (ts->mmap->EVENT_BUF_ADDR >> 8) & 0xFF;
+	ret = CTP_I2C_WRITE(ts->client, I2C_FW_Address, tmp, 3);
+	if (ret < 0) {
+		NVT_LOG("i2c wite error!\n");
+		goto out;
+	}
+	tmp[0] = EVENT_MAP_HOST_CMD;
+	if (angle == 90) {
+		tmp[1] = 0xBC;
+	} else if (angle == 270) {
+		tmp[1] = 0xBB;
+	} else {
+		tmp[1] = 0xBA;
+	}
+	ret = CTP_I2C_WRITE(ts->client, I2C_FW_Address, tmp, 2);
+	if (ret < 0) {
+		NVT_LOG("i2c read error!\n");
+		goto out;
+	}
+	ret = 0;
+
+out:
+	mutex_unlock(&ts->lock);
+	NVT_LOG("--\n");
+	return ret;
+}
+
 #if defined(CONFIG_FB)
 static int fb_notifier_callback(struct notifier_block *self, unsigned long event, void *data)
 {
@@ -1814,6 +1985,12 @@ static int fb_notifier_callback(struct notifier_block *self, unsigned long event
 		blank = evdata->data;
 		if (*blank == FB_BLANK_POWERDOWN) {
 			NVT_LOG("touch suspend\n");
+#ifdef CONFIG_MACH_MI
+			if (!get_lct_tp_work_status()) {
+				suspend_state = true;
+				return 0;
+			}
+#endif
 			flush_work(&g_resume_work);
 			mutex_lock(&ts->pm_mutex);
 			nvt_ts_suspend(&ts->client->dev);
@@ -1823,6 +2000,12 @@ static int fb_notifier_callback(struct notifier_block *self, unsigned long event
 		blank = evdata->data;
 		if (*blank == FB_BLANK_UNBLANK) {
 			NVT_LOG("touch resume\n");
+#ifdef CONFIG_MACH_MI
+			if (!get_lct_tp_work_status()) {
+				suspend_state = false;
+				return 0;
+			}
+#endif
 			schedule_work(&g_resume_work);
 		}
 	}

drivers/input/touchscreen/synaptics_tcm/synaptics_tcm_reflash.c
@@ -34,6 +34,14 @@
 #include <linux/firmware.h>
 #include "synaptics_tcm_core.h"
 
+/* add tp vendor information by wanghan add */
+#include "../lct_tp_info.h"
+/* add tp vendor information by wanghan end */
+
+/* add check F7A LCM by wanghan start */
+extern bool lct_syna_verify_flag;
+/* add check F7A LCM by wanghan end */
+
 #define STARTUP_REFLASH
 
 #define FORCE_REFLASH false
@@ -1934,6 +1942,27 @@ reflash_update(app_firmware)
 
 #define SYNA_TP_INFO_SIZE       128
 
+static int lct_syna_tp_info_update(struct syna_tcm_hcd *tcm_hcd)
+{
+	char tp_info_buf[SYNA_TP_INFO_SIZE];
+	unsigned char *image_config_id;
+	struct app_config_header *header;
+	const unsigned char *app_config_data;
+
+	LOG_ENTRY();
+	app_config_data = reflash_hcd->image_info.app_config.data;
+	header = (struct app_config_header *)app_config_data;
+	image_config_id = header->customer_config_id;
+	memset(tp_info_buf, 0, sizeof(tp_info_buf));
+	sprintf(tp_info_buf, "[Vendor]boe,[FW]0x%c%c,[IC]td4320\n",
+			image_config_id[14],
+			image_config_id[15]);
+	update_lct_tp_info(tp_info_buf, NULL);
+	LOG_DONE();
+	return 0;
+}
+/* add tp vendor information by wanghan end */
+
 static int reflash_do_reflash(void)
 {
 	int retval;
@@ -1996,6 +2025,12 @@ static int reflash_do_reflash(void)
 
 	LOGN(tcm_hcd->pdev->dev.parent,
 			"End of reflash\n");
+	/* add tp vendor information by wanghan add */
+	if (update_area != NONE) {
+		lct_syna_tp_info_update(tcm_hcd);
+		LOGV("Update /proc/tp_info\n");
+	}
+	/* add tp vendor information by wanghan end */
 
 	retval = 0;
 
@@ -2011,12 +2046,134 @@ exit:
 	return retval;
 }
 
+/* add tp vendor information by wanghan add */
+static int lct_syna_tp_lockdown_info_update(char *buf, struct syna_tcm_hcd *tcm_hcd)
+{
+	int retval;
+
+	LOG_ENTRY();
+
+	mutex_lock(&tcm_hcd->extif_mutex);
+	mutex_lock(&reflash_hcd->reflash_mutex);
+
+	retval = reflash_read_data(CUSTOM_OTP, true, NULL);
+	if (retval < 0) {
+		LOGE(tcm_hcd->pdev->dev.parent,
+				"Failed to read lockdown data\n");
+		goto exit;
+	}
+	sprintf(buf, "%02x%02x%02x%02x%02x%02x%02x\n",
+			(uint8_t)(reflash_hcd->read.buf[0]),
+			(uint8_t)(reflash_hcd->read.buf[1]),
+			(uint8_t)(reflash_hcd->read.buf[2]),
+			(uint8_t)(reflash_hcd->read.buf[3]),
+			(uint8_t)(reflash_hcd->read.buf[4]),
+			(uint8_t)(reflash_hcd->read.buf[5]),
+			(uint8_t)(reflash_hcd->read.buf[6]));
+	LOGV("get lockdown info : %s\n", buf);
+
+exit:
+	mutex_unlock(&reflash_hcd->reflash_mutex);
+	mutex_unlock(&tcm_hcd->extif_mutex);
+	LOG_DONE();
+	return 0;
+}
+
+
+#define LCT_SYNA_TP_LOCKDOWN_CALLBACK_RUN_ONLY_ONCE 1
+
+int lct_syna_tp_callback(const char *cmd)
+{
+#if LCT_SYNA_TP_LOCKDOWN_CALLBACK_RUN_ONLY_ONCE
+	static bool run_only_once_flag = false;
+#endif
+	int retval;
+	char tp_lockdown_info_buf[SYNA_TP_INFO_SIZE];
+	struct syna_tcm_hcd *tcm_hcd = reflash_hcd->tcm_hcd;
+
+	LOG_ENTRY();
+
+#if LCT_SYNA_TP_LOCKDOWN_CALLBACK_RUN_ONLY_ONCE
+	if (run_only_once_flag) return 0;
+#endif
+	if(strcmp(cmd, TP_CALLBACK_CMD_LOCKDOWN))
+		return -1;
+	memset(tp_lockdown_info_buf, 0, sizeof(tp_lockdown_info_buf));
+	retval = lct_syna_tp_lockdown_info_update(tp_lockdown_info_buf, tcm_hcd);
+	if (retval < 0) {
+		LOGE(tcm_hcd->pdev->dev.parent, "failed to get tp lockdown info!\n");
+		return retval;
+	}
+	update_lct_tp_info(NULL, tp_lockdown_info_buf);
+#if LCT_SYNA_TP_LOCKDOWN_CALLBACK_RUN_ONLY_ONCE
+	run_only_once_flag = true;
+#endif
+
+	LOG_DONE();
+	return 0;
+}
+
+static int lct_syna_tp_node_init(struct syna_tcm_hcd *tcm_hcd)
+{
+	int retval;
+	char tp_info_buf[SYNA_TP_INFO_SIZE];
+
+	LOG_ENTRY();
+
+	memset(tp_info_buf, 0, sizeof(tp_info_buf));
+	sprintf(tp_info_buf, "[Vendor]boe,[FW]0x%c%c,[IC]td4320\n",
+			tcm_hcd->app_info.customer_config_id[14],
+			tcm_hcd->app_info.customer_config_id[15]);
+	retval = init_lct_tp_info(tp_info_buf, NULL);
+	if (retval < 0) {
+		LOGE(tcm_hcd->pdev->dev.parent, "init_lct_tp_info is failed!\n");
+		return -1;
+	}
+	retval = lct_syna_tp_callback(TP_CALLBACK_CMD_LOCKDOWN);
+	if (retval) {
+		LOGV("ERROR : lct_syna_tp_callback failed!\n");
+	}
+	set_lct_tp_info_callback(lct_syna_tp_callback);
+	LOG_DONE();
+	return 0;
+}
+/* add tp vendor information by wanghan end */
+
 #ifdef STARTUP_REFLASH
 static void reflash_startup_work(struct work_struct *work)
 {
 	int retval;
+
+
+
 	struct syna_tcm_hcd *tcm_hcd = reflash_hcd->tcm_hcd;
+
 	LOG_ENTRY();
+	/* add tp vendor information by wanghan add */
+	retval = lct_syna_tp_node_init(tcm_hcd);
+	if (retval < 0){
+		LOGE(tcm_hcd->pdev->dev.parent,
+				"Failed to initializing tp_info & tp_lockdown_info node\n");
+	}
+	/* add tp vendor information by wanghan end */
+
+#ifdef CONFIG_MACH_MI
+	LOGV("Close the TP FW upgrade feature in longcheer factory version\n");
+	return;
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	pm_stay_awake(&tcm_hcd->pdev->dev);
 
@@ -2240,6 +2397,10 @@ static int __init reflash_module_init(void)
 {
 	int retval;
 	LOG_ENTRY();
+	/* add check F7A LCM by wanghan start */
+	if(!lct_syna_verify_flag)
+		return -ENODEV;
+	/* add check F7A LCM by wanghan end */
 	LOGV("__init reflash module\n");
 	retval = syna_tcm_add_module(&reflash_module, true);
 	if (retval < 0)